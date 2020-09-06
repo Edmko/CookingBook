c1d6148c2a81f715af56eafa9ffaff3dace2a4b2
edmko <edmko195@gmail.com>
2020-09-06 22:54:48 +0300

    add about fragment

--- app/src/main/java/com/edmko/cookingbook/MainActivity.kt
+++ app/src/main/java/com/edmko/cookingbook/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.edmko.cookingbook
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+       navController = this.findNavController(R.id.myNavHostFragment)
+        nav_view.setupWithNavController(navController)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
---
+++ app/src/main/java/com/edmko/cookingbook/ui/about/AboutFragment.kt
@@ -0,0 +1,26 @@
+package com.edmko.cookingbook.ui.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.edmko.cookingbook.R
+import kotlinx.android.synthetic.main.fragment_about.*
+
+class AboutFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        policyWeb.loadUrl("https://rawcdn.githack.com/Edmko/CookingBook/e474f89a01eb54dc4c7cdd0fc610441c722e21d1/Privacy Policy.htm")
+        super.onActivityCreated(savedInstanceState)
+    }
+}
